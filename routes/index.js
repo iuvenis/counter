00ea172d683d1215f713cb25539245b60a5494ef
tpyoung <tylerparkeyoung@gmail.com>
2016-04-30 15:30:42 -1000

    WIP use express router

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express'),
+       router = express.Router();
+
+const counter = require('./counter');
+
+router.use('counter', counter);
+
+module.exports.router;
